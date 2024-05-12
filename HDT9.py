be841ba8b493bf9bbc71c6ebd9cdb9e99e220773
Fercho1118 <rue23748@uvg.edu.gt>
2024-05-12 15:21:00 -0600

    función para mostrar rutas y costos desde un origen especificado
    Se añadió la función que utiliza el grafo para encontrar y mostrar las rutas más cortas desde un origen hasta todos los destinos posibles.

--- HDT9.py
+++ HDT9.py
@@ -3,7 +3,8 @@
 #Felipe Aguilar - 23195
 #Fernando Rueda - 23748
 
-
+import networkx as nx
+import matplotlib.pyplot as plt
 
 def leer_rutas(archivo):
     """ Función para cargar las rutas desde un archivo en un grafo, eliminando espacios extra. """
@@ -16,3 +17,15 @@ def leer_rutas(archivo):
             grafo.add_edge(origen, destino, weight=costo)
             grafo.add_edge(destino, origen, weight=costo)  # Asegura que el grafo sea no dirigido
     return grafo
+
+def mostrar_rutas(grafo, origen):
+    """ Muestra las rutas más cortas desde una estación de origen y las dibuja """
+    try:
+        caminos, costos = nx.single_source_dijkstra(grafo, origen)
+        for destino, ruta in caminos.items():
+            costo = costos[destino]
+            print(f"Desde {origen} a {destino}: Ruta {ruta}, Costo: {costo}")
+    except nx.NetworkXNoPath:
+        print("No hay ruta disponible.")
+    except KeyError:
+        print("Estación no encontrada.")
